7e57fe0997f5b950ede6267cf9c8d9d7eff14551
Dan <dsack7@gmail.com>
2020-03-29 21:27:40 +0200

    Updates

---
+++ precache-manifest.7b1bcfd458a4720c25ba5bf5048ef2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bd2614968952344dbe67ce1a28101a",
+    "url": "/DanPete/react-calculator/index.html"
+  },
+  {
+    "revision": "ab2fc5e67e4db8ea4663",
+    "url": "/DanPete/react-calculator/static/css/main.72f2d982.chunk.css"
+  },
+  {
+    "revision": "350da9316dfb94e9ad49",
+    "url": "/DanPete/react-calculator/static/js/2.09fd4fdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DanPete/react-calculator/static/js/2.09fd4fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2fc5e67e4db8ea4663",
+    "url": "/DanPete/react-calculator/static/js/main.8fe8552c.chunk.js"
+  },
+  {
+    "revision": "e935bd83eddb73206cab",
+    "url": "/DanPete/react-calculator/static/js/runtime-main.ff0e3701.js"
+  }
+]);
